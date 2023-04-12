32731e90eb20ab68d96383377b44fbe665e53cb0
Sheuh Xiong <sheuhxiong@gmail.com>
2023-04-12 12:00:18 -0400

    Add stage5 solution

--- src/main/java/com/amazingnumbers/app/stage5/Main.java
+++ src/main/java/com/amazingnumbers/app/stage5/Main.java
@@ -1,17 +1,16 @@
-package com.amazingnumbers.app.stage4;
+package com.amazingnumbers.app.stage5;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     private String[] strings;
-    private long[] numbers;
+    private long[] numbersInput;
     private boolean twoNaturalNumbers;
-
-    private Main(String[] _stringInput) {
-        strings = _stringInput;
-        numbers = new long[_stringInput.length];
-        twoNaturalNumbers = _stringInput.length > 1;
-    }
+    private String propertyInput;
+    private String[] properties = { "BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY", "EVEN", "ODD" };
 
     public static void main(String[] args) {
         printWelcome();
@@ -23,24 +22,44 @@ public class Main {
         System.out.println("Goodbye!");
     }
 
+    private Main(String[] _stringInput) {
+        strings = _stringInput;
+        // Minus 1 since we don't want to account for the property input string)
+        numbersInput = new long[(_stringInput.length == 3) ? _stringInput.length - 1 : _stringInput.length];
+        twoNaturalNumbers = _stringInput.length > 1;
+    }
+
     private static int run() {
         Main app = new Main(getInput());
 
-        if (app.strings[0].isEmpty()) {
-            printInstructions();
+        // Handle edge case where user inputs empty line
+        if (app.strings[0].isEmpty()) printInstructions();
+
+        // Update propertyInput if there is a 3rd string
+        if (app.strings.length == 3) {
+            String tmpStr = app.strings[2].toUpperCase();
+            boolean found = Arrays.stream(app.properties).anyMatch(
+                    str -> str.equals(tmpStr));
+            if (!found) {
+                app.printIncorrectProperty(tmpStr);
+                return 0;
+            } else {
+                app.propertyInput = tmpStr;
+            }
         }
 
         // Terminate app
         if (!app.twoNaturalNumbers && app.strings[0].equals("0")) return -1;
 
-        for (int i = 0; i < app.numbers.length; i++) {
+        // Update the numbersInput
+        for (int i = 0; i < app.numbersInput.length; i++) {
             try {
                 // Map the strings to long values
                 long result = Long.parseLong(app.strings[i]);
                 if (result < 0) {
                     throw new NumberFormatException();
                 } else {
-                    app.numbers[i] = result;
+                    app.numbersInput[i] = result;
                 }
             } catch (NumberFormatException e) {
                 if (i == 0) {
@@ -51,6 +70,7 @@ public class Main {
                 return 0;
             }
         }
+
         app.printProperties();
         System.out.print("\n");
         return 0;
@@ -58,11 +78,20 @@ public class Main {
 
     private void printProperties() {
         // Get the first parameter of the input: starting number
-        Long n = numbers[0];
+        Long n = numbersInput[0];
 
         // Get the second parameter of the input: consecutive numbers
-        long length = (twoNaturalNumbers) ? numbers[1] : 1L;
+        long length = (twoNaturalNumbers) ? numbersInput[1] : 1L;
 
+        if (twoNaturalNumbers && strings.length == 3) {
+            // Add logic to process 3rd user request
+            printThirdOption(n, length, propertyInput);
+        } else {
+            printFirstSecondOption(n, length);
+        }
+    }
+
+    private void printFirstSecondOption(long n, long length) {
         for (int i = 0; i < length; i++) {
             // Store booleans into a LinkedHashMap since we need to filter for true values later
             long currentNum = n + i;
@@ -71,6 +100,7 @@ public class Main {
             boolMap.put("duck", getDuck(currentNum));
             boolMap.put("palindromic", getPalindromic(currentNum));
             boolMap.put("gapful", getGapful(currentNum));
+            boolMap.put("spy", getSpy(currentNum));
             boolMap.put("even", getEven(currentNum));
             boolMap.put("odd", !getEven(currentNum));
 
@@ -83,6 +113,7 @@ public class Main {
                         duck: %b
                  palindromic: %b
                       gapful: %b
+                         spy: %b
                         even: %b
                          odd: %b""",
                         strNum,
@@ -90,6 +121,7 @@ public class Main {
                         boolMap.get("duck"),
                         boolMap.get("palindromic"),
                         boolMap.get("gapful"),
+                        boolMap.get("spy"),
                         boolMap.get("even"),
                         boolMap.get("odd")));
             } else {
@@ -111,6 +143,46 @@ public class Main {
         }
     }
 
+    private void printThirdOption(long n, long length, String propertyInput) {
+        int i = 0;
+        int currentNum = (int) n;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (i < length) {
+            Map<String, Boolean> boolMap = new LinkedHashMap<>();
+            boolMap.put("buzz", getBuzz(currentNum));
+            boolMap.put("duck", getDuck(currentNum));
+            boolMap.put("palindromic", getPalindromic(currentNum));
+            boolMap.put("gapful", getGapful(currentNum));
+            boolMap.put("spy", getSpy(currentNum));
+            boolMap.put("even", getEven(currentNum));
+            boolMap.put("odd", !getEven(currentNum));
+
+            boolean propertyIsTrue = boolMap.get(propertyInput.toLowerCase());
+
+            if (propertyIsTrue) {
+                StringBuilder strBuilder = new StringBuilder();
+                String strNum = addCommasToLong(currentNum);
+                strBuilder.append(String.format("%s is", strNum));
+                for (Map.Entry<String, Boolean> entry : boolMap.entrySet()) {
+                    // If the value is true, then we add it to the StringBuilder
+                    if (entry.getValue()) {
+                        strBuilder.append(String.format(" %s,", entry.getKey()));
+                    }
+                }
+                String myString = strBuilder.toString();
+                // Remove unwanted comma at end of the string
+                myString = myString.substring(0, myString.length() - 1);
+                stringBuilder.append(myString + "\n");
+                i++;
+            }
+
+            currentNum++;
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
     private static String[] getInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a request: ");
@@ -125,14 +197,23 @@ public class Main {
 
     private static void printInstructions() {
         System.out.println("""
-                Supported requests:
-                - enter a natural number to know its properties;
-                - enter two natural numbers to obtain the properties of the list:
-                  * the first parameter represents a starting number;
-                  * the second parameter shows how many consecutive numbers are to be printed;
-                - separate the parameters with one space;
-                - enter 0 to exit.
-                """);
+        Supported requests:
+        - enter a natural number to know its properties;
+        - enter two natural numbers to obtain the properties of the list:
+          * the first parameter represents a starting number;
+          * the second parameter shows how many consecutive numbers are to be printed;
+        - two natural numbers and a property to search for;
+        - separate the parameters with one space;
+        - enter 0 to exit.
+        """);
+    }
+
+    private static void printIncorrectProperty(String propertyInput) {
+        // Change [SUN] with instance variable
+        System.out.println(String.format("""
+                The property [%s] is wrong.
+                Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY]
+                """, propertyInput));
     }
 
     private boolean getBuzz(long n) {
@@ -177,6 +258,19 @@ public class Main {
 
     private boolean getEven(long n) {
         return (n % 2 == 0);
+    }
+
+    private boolean getSpy(long n) {
+        String numStr = String.valueOf(n);
+        long sum = 0;
+        long product = 1L;
+
+        for (int i = 0; i < numStr.length(); i++) {
+            long digit = Character.getNumericValue(numStr.charAt(i));
+            sum += digit;
+            product *= digit;
+        }
+        return (sum == product);
     }
 
     private String addCommasToLong(long n) {
